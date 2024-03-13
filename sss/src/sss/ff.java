46ad3355d8cdf17c22c6d2c68c476937eb1faaee
ParkBumJun <bombjuun@naver.com>
2024-03-13 16:42:51 +0900

    두번째 커밋

--- sss/src/sss/ff.java
+++ sss/src/sss/ff.java
@@ -4,6 +4,11 @@ public class ff {
 
 	public static void main(String[] args) {
 		System.out.println("github 연동");
+		System.out.println("github 연동");
+		System.out.println("github 연동");
+		System.out.println("github 연동");
+		System.out.println("github 연동");
+		System.out.println("github 연동");
 	}
 
 }
